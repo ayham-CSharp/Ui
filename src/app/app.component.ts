ee6fa39db2ecf52346db715ceeb8e1241184d1df
Ayham <ayhamalama@gmail.com>
2023-01-10 03:37:28 +0200

    AngularT

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Home - Lavandar Code';
+  constructor(private tit:Title){
+    tit.setTitle("Home");
+  }
+}
